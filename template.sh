3ce78ed9c278deb1a75b2f5ba0fd234ea29f22f5
sakshimp2004 <sakshimp2004@gmail.com>
2026-05-04 16:20:21 +1200

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating Directories
+mkdir -p src
+mkdir -p reseach
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reseach/analysis.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully."
